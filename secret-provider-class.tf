9ffa3158ca8f50a04fa882343803a3775546e5b2
aumthakkar <aumthakkar@yahoo.co.uk>
2026-03-01 10:02:51 +0000

    added the spc crd once again after installing both the secret-mgr drivers

--- secret-provider-class.tf
+++ secret-provider-class.tf
@@ -1,34 +1,34 @@
-# resource "kubernetes_manifest" "secret_provider_class_manifest" {
-#   depends_on = [
-#     helm_release.secrets_store_csi_driver,
-#     aws_eks_addon.ascp_addon
-#    ]
+resource "kubernetes_manifest" "secret_provider_class_manifest" {
+  depends_on = [
+    helm_release.secrets_store_csi_driver,
+    aws_eks_addon.ascp_addon
+   ]
 
-#   manifest = {
-#     apiVersion = "secrets-store.csi.x-k8s.io/v1"
-#     kind = "SecretProviderClass"
+  manifest = {
+    apiVersion = "secrets-store.csi.x-k8s.io/v1"
+    kind = "SecretProviderClass"
 
-#     metadata = {
-#       name = "db-secrets"
-#     }
+    metadata = {
+      name = "db-secrets"
+    }
 
-#     spec = {
-#       provider = "aws"
+    spec = {
+      provider = "aws"
 
-#       parameters = {
-#         usePodIdentity = "true"
+      parameters = {
+        usePodIdentity = "true"
         
-#         objects = <<-EOT
-#           - objectName: "db-secret-1"
-#             objectType: "secretManager"
-#             jmesPath: 
-#               - path : "MYSQL_USER"
-#                 objectAlias: "MYSQL_USER"
-#               - path: "MYSQL_PASSWORD"
-#                 objectAlias: "MYSQL_PASSWORD"
+        objects = <<-EOT
+          - objectName: "db-secret-1"
+            objectType: "secretManager"
+            jmesPath: 
+              - path : "MYSQL_USER"
+                objectAlias: "MYSQL_USER"
+              - path: "MYSQL_PASSWORD"
+                objectAlias: "MYSQL_PASSWORD"
 
-#         EOT
-#       }
-#     }
-#   }
-# }
+        EOT
+      }
+    }
+  }
+}
